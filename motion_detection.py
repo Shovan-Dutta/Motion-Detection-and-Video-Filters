7d790067763333d1cbbfa05d1f899bfe29b8f100
Shovan-Dutta <73119615+Shovan-Dutta@users.noreply.github.com>
2023-04-15 19:30:52 +0530

    Update motion_detection.py

--- motion_detection.py
+++ motion_detection.py
@@ -6,19 +6,20 @@ camera = cv.VideoCapture(0)
 _, frame1 = camera.read()
 _, frame2 = camera.read()
 
+
 while True:
 
     #live video motion detection using contours
-    diff = cv.absdiff(frame1, frame2)
-    gray = cv.cvtColor(diff,cv.COLOR_BGR2GRAY)
-    blur = cv.GaussianBlur(gray,(5,5),0)
-    _, thresh = cv.threshold(blur,20,255,cv.THRESH_BINARY)
-    dilated  = cv.dilate (thresh, None, iterations = 3)
-    contours, _ = cv.findContours(dilated,cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    diff = cv.absdiff(frame1, frame2) #Calculates the per-element absolute difference between two frames
+    gray = cv.cvtColor(diff,cv.COLOR_BGR2GRAY) #converts the difference frame to gray image
+    blur = cv.GaussianBlur(gray,(5,5),0) #to reduce noise in the difference frame
+    _, thresh = cv.threshold(blur,20,255,cv.THRESH_BINARY) # getting a binary image
+    dilated  = cv.dilate (thresh, None, iterations = 3) # determines the shape of a pixel neighborhood 
+    contours, _ = cv.findContours(dilated,cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE) #The function retrieves contours from the binary image
 
     for contour in contours:
         if cv.contourArea(contour)>200:
-            cv.drawContours(frame1, contours, -1, (0,0,255),2)
+            cv.drawContours(frame1, contours, -1, (0,0,255),2) # highlights the movement area
             cv.putText(frame1,'Status: {}'.format('Movement'),(10,40),cv.FONT_ITALIC,1,(0,0,255), 3)
         cv.imshow('Camera',frame1)
     frame1 = frame2
